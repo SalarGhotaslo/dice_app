07e0e8e6bc7da16e7e2d3d8575327370b794dbf6
Salar Ghotaslo <Salar_@live.co.uk>
2021-01-10 21:39:58 +0000

    dice app finished

--- lib/roll_dice.rb
+++ lib/roll_dice.rb
@@ -1,5 +1,23 @@
 class Roll_dice
-    def roll
-        return rand(5) + 1
+
+    def initialize
+        @scores = []
     end
+    
+    def roll(n)
+        rolled_results = []
+        n.times{results = rand(1..6)
+            @scores << results
+            rolled_results << results}
+            return rolled_results
+    end
+
+    def record
+        @scores
+    end
+
+    def current_score
+        @scores.sum
+    end
+
 end
--- spec/roll_dice_spec.rb
+++ spec/roll_dice_spec.rb
@@ -3,13 +3,36 @@ require 'roll_dice'
 describe Roll_dice do
     it 'able to roll dice' do
         roll_dice = Roll_dice.new
-        expect(roll_dice). to respond_to(:roll)
+        expect(roll_dice). to respond_to(:roll).with(1).argument
     end
     it 'rolled dice should return a number 1-6' do
         roll_dice = Roll_dice.new
         # expect(roll_dice). to respond_to(:roll).with(1).argument
-        expect(roll_dice.roll).to be_between(1,6)
+        expect(roll_dice.roll(1)[0]).to be_between(1,6)
     end
-    
+    it 'returns a random number' do
+        srand(6)
+        expect(subject.roll(1)[0]).to eq 3
+    end
 
+    it 'allows us to roll any number of dice' do
+        srand(12)
+        expect(subject.roll(2)[0]).to eq 4
+    end
+
+    it 'records all of our rolls' do
+        srand(12)
+        expect(subject.roll(2)).to eq [4,4]
+    end
+    it 'records each dice roll' do
+        srand(6)
+        subject.roll(3)
+        expect(subject.record).to eq [3,2,4]
+    end
+
+    it 'displays current score' do
+        srand(6)
+        subject.roll(3)
+        expect(subject.current_score).to eq 9
+    end
 end
